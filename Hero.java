0d6d7f6393cac5fac5da77ab342e0aa52e3bb740
349500348 <349500348@gapps.yrdsb.ca>
2024-05-29 11:31:53 -0400

    Update Hero.java, Update MyWorld.java

--- Hero.java
+++ Hero.java
@@ -8,12 +8,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Hero extends Actor
 {
+    private GreenfootImage image;
+    
+    public Hero()
+    {
+        image = getImage();
+        resize();
+    }
+    public void resize()
+    {
+        GreenfootImage img = new GreenfootImage("spaceship_idle.png");
+        img.scale(img.getWidth()/10, img.getHeight()/10);
+        setImage(img);
+    }
     /**
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        
     }
 }
