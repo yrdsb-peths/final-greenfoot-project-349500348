1975abf1d947da75f7ff7c1293756a7d937e8d52
349500348 <349500348@gapps.yrdsb.ca>
2024-06-05 11:42:35 -0400

    Added enemy shooting and enemy laser collision, works properly

--- EnemyLaser.java
+++ EnemyLaser.java
@@ -12,8 +12,46 @@ public class EnemyLaser extends Actor
      * Act - do whatever the Enemy_Laser wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private GreenfootImage image;
+
+    /**
+     * Act - do whatever the laser wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public EnemyLaser()
+    {
+        setRotation(180);
+        image = getImage();
+        resize();
+    }
+    public void resize()
+    {
+        GreenfootImage img = new GreenfootImage("enemy_laser.png");
+        img.scale(img.getWidth()/4, img.getHeight()/4);
+        setImage(img);
+    }
+    
+    private int speed = 4;
     public void act()
     {
-        // Add your action code here.
+        // Move object at selected speed
+        move(speed);
+        // Remove object when off screen
+        if (isAtEdge())
+        {
+            getWorld().removeObject(this);
+        }
+        else 
+        {
+            kill();
+        }
+    }
+    public void kill()
+    {
+        if(isTouching(Hero.class))
+            {
+                removeTouching(Hero.class);
+                getWorld().removeObject(this);
+            }
     }
 }
--- enemy.java
+++ enemy.java
@@ -12,19 +12,32 @@ public class Enemy extends Actor
      * Act - do whatever the enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private GreenfootImage image;
     private int speed = 2;
+    private int shootTimer;
+    private final int shootSpeed = 150;
     
     public Enemy()
     {
         // rotate image to face hero
+        image = getImage();
         setRotation(90);
         resize();
+        shootTimer = 0;
     }
     
     public void act()
     {
         setLocation(getX() - speed, getY());
-        
+        if (shootTimer > 0)
+        {
+            shootTimer--;
+        }
+        if (shootTimer == 0)
+        {
+            shoot();
+            shootTimer = shootSpeed;
+        }
         // Remove object when off screen to reduce lag
         if (getX() == 0)
         {
@@ -39,4 +52,20 @@ public class Enemy extends Actor
         setImage(img);
     }
     
+    public void shoot()
+    {
+
+        // Creates a Laser object
+        EnemyLaser enemyLaser = new EnemyLaser();
+        
+        // Gets world ship is in
+        World world = getWorld();
+        
+        // Set location of laser to tip of the ship
+        int x = getX() + (image.getWidth()/4) / 2;
+        int y = getY();
+        
+        // Add laser object at selected position
+        world.addObject(enemyLaser, x, y);
+    }
 }
