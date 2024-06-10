dbe7688e320107d26268b18a043c9c7f9d64163e
349500348 <349500348@gapps.yrdsb.ca>
2024-06-10 12:05:24 -0400

    Added comments to hero and moved some stuff around to make it make more sense

--- Hero.java
+++ Hero.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
 
 /**
  * Write a description of class Hero here.
@@ -8,62 +8,59 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */ 
 public class Hero extends Actor
 {
-    private GreenfootImage image;
-    private int shootTimer;
-    private final int shootSpeed = 20;
-    private final int speed = 4;
+    private int shootTimer; // variable to count frames between shots
+    private final int shootSpeed = 20; // number of frames before Hero can shoot again
+    private final int speed = 4; // number of pixels the Hero moves per frame while key is pressed
     
     public Hero()
     {
-        image = getImage();
-        resize();
-        shootTimer = 0; 
+        resize(); // resize the Hero's image 
+        shootTimer = 0; // initializes the shoot timer to 0
     }
+    
     public void resize()
     {
-        GreenfootImage img = new GreenfootImage("spaceship_idle.png");
-        img.scale(img.getWidth()/15, img.getHeight()/15);
-        setImage(img);
+        GreenfootImage img = new GreenfootImage("spaceship_idle.png"); // load the spaceship image
+        img.scale(img.getWidth() / 15, img.getHeight() / 15); // scale down the image
+        setImage(img); // set the scaled image as the Hero's image
     }
+    
     /**
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        if (shootTimer > 0)
+        if (shootTimer > 0) // check if the shoot timer is greater than 0
         {
-            shootTimer--;
+            shootTimer--; // decrement the shoot timer
         }
-        if (Greenfoot.isKeyDown("space") && shootTimer == 0)
+        if (Greenfoot.isKeyDown("space") && shootTimer == 0) // check if space key is pressed and shoot timer is 0
         {
-            shoot();
-            shootTimer = shootSpeed;
+            shoot(); // call the shoot method
+            shootTimer = shootSpeed; // reset the shoot timer
         }
-        if(Greenfoot.isKeyDown("up"))
+        if (Greenfoot.isKeyDown("up")) // check if the up key is pressed
         {
-            setLocation(getX(), getY() - speed);
+            setLocation(getX(), getY() - speed); // move the Hero up
         }
-        else if(Greenfoot.isKeyDown("down"))
+        else if (Greenfoot.isKeyDown("down")) // check if the down key is pressed
         {
-            setLocation(getX(), getY() + speed);
+            setLocation(getX(), getY() + speed); // move the Hero down
         }
     }
     
     public void shoot()
     {
-
-        // Creates a Laser object
-        Laser laser = new Laser();
+        Laser laser = new Laser(); // creates a Laser object
         
-        // Gets world ship is in
-        World world = getWorld();
+        World world = getWorld(); // gets the world the Hero is in
         
-        // Set location of laser to tip of the ship
-        int x = getX() + (image.getWidth()/15) / 2;
+        // calculates the location to place the laser at the tip of the ship
+        GreenfootImage image = getImage(); // gets the current image of the Hero
+        int x = getX() + (image.getWidth() / 15) / 2;
         int y = getY();
         
-        // Add laser object at selected position
-        world.addObject(laser, x, y);
+        world.addObject(laser, x, y); // adds the laser object to the world at the calculated position
     }
 }
